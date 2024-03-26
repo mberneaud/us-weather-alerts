be83504a68770b015ad41451995cd3830ec49d83
mouadrabihsenhaji <mouadrabihsenhaji@gmail.com>
2024-03-26 12:56:50 +0000

    upload_to_gvs

--- us_weather_alerts/api.py
+++ us_weather_alerts/api.py
@@ -15,6 +15,7 @@ from typing import List, Dict
 from google.cloud import storage
 
 
+
 def scrape_alerts(status: str, start_datetime: datetime, end_datetime: datetime = datetime.now(), limit: int = 500) -> List[Dict]:
     """Scrapes alerts for the specified timeframe and status and returns a list of dictionaries, where each dictionary represents
     an alert (called Feature in the API response)
@@ -107,22 +108,29 @@ def upload_to_gcp(dataframe: pd.DataFrame, project_id: str, table_id, keyfile_pa
     pandas_gbq.to_gbq(dataframe, table_id, project_id=project_id, if_exists="append", credentials=credentials)
 
 
-def upload_to_gcs_from_memory(dataframe: pd.DataFrame):
+def upload_to_gcs_from_memory(dataframe: pd.DataFrame ,bucket_name : str,key_file_path: str):
+    """Uploads a file to the bucket."""
+
+    # Convert DataFrame to string representation
+    contents = dataframe.to_csv(index=False)
 
     date_today = datetime.now()
     year = date_today.strftime("%Y")
     month = date_today.strftime("%m")
     day = date_today.strftime("%d")
 
-    blob_path = f"us_weather/alerts/{year}/{month}/{day}/{file_name}"
+    blob_path = f"us_weather_alerts/{year}_{month}_{day}"
 
-    storage_client = storage.Client()
+    # Create credentials from service account key file
+    credentials = service_account.Credentials.from_service_account_file(key_file_path)
 
-    bucket_name = os.environ["US_WEATHER_BUCKET_ALERTS"]
+    # Create storage client with provided credentials
+    storage_client = storage.Client(credentials=credentials)
+
     bucket = storage_client.bucket(bucket_name)
 
     blob = bucket.blob(blob_path)
-    blob.upload_from_string(dataframe)
+    blob.upload_from_string(contents , 'text/csv')
 
     print(f"{blob_path} uploaded to {bucket_name}.")
 
@@ -137,6 +145,10 @@ if __name__ == "__main__":
     gcp_project_id = os.environ["GCP_PROJECT_ID"]
     table_id = f"{os.environ['BRONZE_DATASET']}.alerts"
 
+    #GCS upload parameters
+    bucket_name = os.environ["US_WEATHER_AlERTS_BUCKET"]
+
     features = scrape_alerts(status="actual", start_datetime=start_datetime, limit=LIMIT)
     df = parse_features(features)
-    upload_to_gcp(df, gcp_project_id, table_id, keyfile_path)
+    #upload_to_gcp(df, gcp_project_id, table_id, keyfile_path)
+    upload_to_gcs_from_memory(df, bucket_name , keyfile_path)
