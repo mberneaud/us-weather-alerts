081bd0928fbe8109b8067d577dfb275a04f9ba5a
mouadrabihsenhaji <mouadrabihsenhaji@gmail.com>
2024-03-27 22:03:25 +0000

    load to bigquery fucntion implementation init

--- us_weather_alerts/load_to_bigquery.py
+++ us_weather_alerts/load_to_bigquery.py
@@ -1,28 +1,42 @@
 from google.cloud import bigquery
+import json
+import os
+from google.oauth2 import service_account
 
 def load_to_bigquery():
- # Construct a BigQuery client object.
- client = bigquery.Client()
+
+ keyfile_path = os.environ["SERVICE_ACCOUNT_KEYFILE"]
+
+ credentials = service_account.Credentials.from_service_account_file(keyfile_path)
+
+ client = bigquery.Client(credentials=credentials)
 
  # TODO(developer): Set table_id to the ID of the table to create.
  # table_id = "your-project.your_dataset.your_table_name"
+ table_id = f"{os.environ['BRONZE_DATASET']}.alerts"
+
+ # Read schema from JSON file
+ with open('schemas/alerts.json') as f:
+    schema_json = json.load(f)
+
+ # Define BigQuery table schema
+ schema = []
+ for field in schema_json:
+    schema.append(bigquery.SchemaField(field['name'], field['type'],field['mode']))
 
  job_config = bigquery.LoadJobConfig(
-    schema=[
-        bigquery.SchemaField("name", "STRING"),
-        bigquery.SchemaField("post_abbr", "STRING"),
-    ],
+    schema=schema,
     skip_leading_rows=1,
     # The source format defaults to CSV, so the line below is optional.
-    source_format=bigquery.SourceFormat.CSV,
+    source_format=bigquery.SourceFormat.CSV
  )
- uri = "gs://cloud-samples-data/bigquery/us-states/us-states.csv"
+ uri = "gs://us_weather_alerts/us_weather_alerts/2024_03_26"
 
  load_job = client.load_table_from_uri(
     uri, table_id, job_config=job_config
- )  # Make an API request.
+ )
 
- load_job.result()  # Waits for the job to complete.
+ load_job.result()
 
- destination_table = client.get_table(table_id)  # Make an API request.
+ destination_table = client.get_table(table_id)
  print("Loaded {} rows.".format(destination_table.num_rows))
