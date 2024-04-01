a6b121b2575f3870951a489009347e86163f3046
mouadrabihsenhaji <mouadrabihsenhaji@gmail.com>
2024-04-01 12:24:54 +0000

    implment dag functions

--- dags/retrieve_raw_data.py
+++ dags/retrieve_raw_data.py
@@ -1,25 +1,34 @@
 import os
-
 from airflow import DAG
-
 import pendulum
 from airflow.operators.bash import BashOperator
+from datetime import datetime, timezone
+from us_weather_alerts import api, load_to_bigquery
 
-
-from us_weather_alerts import api , load_to_bigquery
+keyfile_path = os.environ["SERVICE_ACCOUNT_KEYFILE"]
+gcp_project_id = os.environ["GCP_PROJECT_ID"]
+table_id = f"{os.environ['BRONZE_DATASET']}.alerts"
+bucket_name = os.environ["US_WEATHER_AlERTS_BUCKET"]
 
 
 def fetch_and_push_alerts_to_gcs():
     """
     Fetches alerts data and pushes it to Google Cloud Storage (GCS).
     """
-    # Your code to fetch alerts data and push to GCS goes here
-    pass
+    # API request parameters
+    LIMIT = 500
+    start_datetime = datetime(2024, 1, 1, 1, 0, 0, 1)
+    features = api.scrape_alerts(
+        status="actual", region_type="land", start_datetime=start_datetime, limit=LIMIT
+    )
+    df = api.parse_features(features)
+    api.upload_to_gcs_from_memory(df, bucket_name, keyfile_path)
 
 
 def load_most_recent_file_from_gcs_to_bigquery():
     """
     Loads the most recent file from Google Cloud Storage (GCS) to BigQuery alerts table.
     """
-    # Your code to load the most recent file from GCS to BigQuery bronze table goes here
-    pass
+    load_to_bigquery.transfer_recent_file_to_bigquery(
+        bucket_name, table_id, keyfile_path
+    )
