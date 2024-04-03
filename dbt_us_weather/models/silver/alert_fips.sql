1dd8e27a5cebc3f9b9fe805d4619c5b3396d91d3
janinewin <144950017+janinewin@users.noreply.github.com>
2024-04-03 23:19:44 +0200

    Update alert_fips.sql
    tested SQL query on big query dataset bronze and silver
    creates the unnested alert_fips mapping table that can be used for gold layer, to create the overall view

--- dbt_us_weather/models/silver/alert_fips.sql
+++ dbt_us_weather/models/silver/alert_fips.sql
@@ -1,4 +1,10 @@
-SELECT
-    id AS alert_id,
-    UNNEST(RIGHT(same, 5)) AS fips_id
-FROM {{ source('us_weather_alerts_bronze', 'alerts') }}
+SELECT 
+    af.alert_id, 
+    RIGHT(REPLACE(f, '"', ''),5) AS fips_id
+FROM (
+    SELECT 
+        id AS alert_id, 
+        JSON_EXTRACT_ARRAY(fips) AS fips_id
+    FROM {{ source('us_weather_alerts_bronze', 'alerts') }}
+) af
+, UNNEST(af.fips_id) f
