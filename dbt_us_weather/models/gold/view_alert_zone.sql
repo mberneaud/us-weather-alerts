77b9dc352f1b0cbf2c1c41fc3428c349eb16f8c4
janinewin <144950017+janinewin@users.noreply.github.com>
2024-04-03 23:55:37 +0200

    Create view_alert_zone.sql

---
+++ dbt_us_weather/models/gold/view_alert_zone.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    materialized='view',
+  )
+}}
+
+SELECT
+  alert_id AS AlertID,
+  fips_id AS FIPS,
+  state_name_abbr AS State,
+  county_name AS County,
+  zone_name AS Zone,
+  event AS Event,
+  event_start AS Start,
+  event_end AS End,
+  alert_severity AS Severity,
+  alert_urgency AS Urgency,
+  alert_certainty AS Certainty,
+  recommended_action_type AS RecommendedAction,
+  recommended_action_description AS RecommendedActionDescription
+  
+FROM {{ ref('alert_zone') }}
