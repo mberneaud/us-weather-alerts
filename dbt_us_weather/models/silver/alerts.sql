f67f2b49f0261fb87782b845b325d306e8692be1
Janine Windhoff <janine.windhoff@proton.me>
2024-04-04 19:28:03 +0000

    edited the alerts silver model to make sure that each alert_id is unique and always the most recent sent date ist chosen

--- dbt_us_weather/models/silver/alerts.sql
+++ dbt_us_weather/models/silver/alerts.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT
+SELECT
   id AS alert_id,
   event,
   CASE WHEN onset <> 'None' THEN PARSE_TIMESTAMP('%FT%T%Ez', onset) ELSE NULL END AS event_start,
@@ -13,5 +13,9 @@ SELECT DISTINCT
   urgency AS alert_urgency,
   response AS recommended_action_type,
   instruction AS recommended_action_description
-
-FROM {{ source('us_weather_alerts_bronze', 'alerts') }}
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY id ORDER BY sent DESC) AS row_num
+    FROM {{ source('us_weather_alerts_bronze', 'alerts') }}
+) AS ranked
+WHERE row_num = 1;
