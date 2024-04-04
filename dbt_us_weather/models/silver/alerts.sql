4fd731c464df21351d90294b1e754a21e45221e8
Janine Windhoff <janine.windhoff@proton.me>
2024-04-04 15:44:55 +0000

    removing transformation of ISO date string to datetime in order to run dbt model

--- dbt_us_weather/models/silver/alerts.sql
+++ dbt_us_weather/models/silver/alerts.sql
@@ -1,10 +1,10 @@
 SELECT
   id AS alert_id,
   event,
-  DATETIME(onset) AS event_start,
-  DATETIME(ends) AS event_end,
-  DATETIME(effective) AS information_start,
-  DATETIME(expires) AS information_end,
+  onset AS event_start,
+  ends AS event_end,
+  effective AS information_start,
+  expires AS information_end,
   status AS alert_status,
   messageType AS alert_message_type,
   severity AS alert_severity,
@@ -12,5 +12,5 @@ SELECT
   urgency AS alert_urgency,
   response AS recommended_action_type,
   instruction AS recommended_action_description
-  
+
 FROM {{ source('us_weather_alerts_bronze', 'alerts') }}
