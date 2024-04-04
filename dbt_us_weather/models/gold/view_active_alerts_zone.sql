7dfa0c9826f84e2a8c3ba59a33625a74dfed5396
Janine Windhoff <janine.windhoff@proton.me>
2024-04-04 20:16:45 +0000

    updating the reference in gold layer view after renaming the models

--- dbt_us_weather/models/gold/view_active_alerts_zone.sql
+++ dbt_us_weather/models/gold/view_active_alerts_zone.sql
@@ -19,4 +19,4 @@ SELECT
   recommended_action_type AS RecommendedAction,
   recommended_action_description AS RecommendedActionDescription
 
-FROM {{ ref('alerts_zone') }}
+FROM {{ ref('active_alerts_zone') }}
